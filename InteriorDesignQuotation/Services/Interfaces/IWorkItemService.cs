bc991772053d48be5d7364d8094d05066f00d7f3
GENYA <t6589989@gmail.com>
2022-09-21 16:53:01 +0800

    feat: Add view model and work item service
    Add notify property base to WPF library
    Add relay command to WPF library
    Main project add WPF library reference

---
+++ InteriorDesignQuotation/Services/Interfaces/IWorkItemService.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.ObjectModel;
+using WPFLibrary;
+
+namespace InteriorDesignQuotation.Services.Interfaces;
+
+public interface IWorkItemService
+{
+    ObservableCollection<WorkItemViewModel> GetWorkItem();
+}
+
+public class WorkItemViewModel : NotifyPropertyBase
+{
+    public string Category { get; set; } = string.Empty;
+
+    public string Area { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Quantity { get; set; }
+
+    public string Unit { get; set; } = string.Empty;
+
+    public decimal UnitPrice { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public string Note { get; set; } = string.Empty;
+}
+
+internal class WorkItemService : IWorkItemService
+{
+    public ObservableCollection<WorkItemViewModel> GetWorkItem()
+    {
+        return new ObservableCollection<WorkItemViewModel>
+        {
+            new()
+            {
+                Category = "拆除工程",
+                Area = "前陽台",
+                Name = "地磚",
+                Quantity = 5,
+                Unit = "坪",
+                UnitPrice = 1600,
+                TotalPrice = 8000,
+                Note = "牆面壁磚破損疑慮，泥作回補，有補洞痕跡，地面孔洞待確認"
+            },
+            new()
+            {
+                Category = "拆除工程",
+                Area = "前陽台",
+                Name = "落地門窗",
+                Quantity = 1,
+                Unit = "式",
+                UnitPrice = 3500,
+                TotalPrice = 3500,
+                Note = string.Empty
+            },
+            new()
+            {
+                Category = "拆除工程",
+                Area = "大浴室",
+                Name = "地磚",
+                Quantity = 1.5m,
+                Unit = "坪",
+                UnitPrice = 1600,
+                TotalPrice = 2400,
+                Note = string.Empty
+            },
+            new()
+            {
+                Category = "泥作工程",
+                Area = "大浴室",
+                Name = "牆面防水",
+                Quantity = 8,
+                Unit = "坪",
+                UnitPrice = 1500,
+                TotalPrice = 12000,
+                Note = string.Empty
+            }
+        };
+    }
+}
