3506f1a0abcb8bfd20a2c72351640254aa223ce0
GENYA <t6589989@gmail.com>
2022-09-26 13:50:02 +0800

    fix: outer control no update when data grid cell changed
    Add decimal converter to avoid the problem that text box backspace to
    empty and input number will get stuck
    Fix total price no update when add work item
    Add Microsoft.Xaml.Behavior Nuget
    Work item data grid use interaction trigger to resolve outer control no
    update when data grid cell changed
    WPFLibrary rename to BindingLibrary

--- BindingLibrary/NotifyPropertyBase.cs
+++ BindingLibrary/NotifyPropertyBase.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace WPFLibrary;
+namespace BindingLibrary;
 
 public class NotifyPropertyBase : INotifyPropertyChanged
 {
