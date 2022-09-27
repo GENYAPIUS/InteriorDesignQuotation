b858ad75a396a30c04c6e4bf25ff1c2ae81ec102
antinazoa <antinazoa@02003-019232-03>
2022-09-27 13:18:51 +0800

    新增Partnerview

---
+++ InteriorDesignQuotation/PartnerPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace InteriorDesignQuotation
+{
+    /// <summary>
+    /// PartnerPage.xaml 的互動邏輯
+    /// </summary>
+    public partial class PartnerPage : Page
+    {
+        public PartnerPage()
+        {
+          InitializeComponent();
+
+            //check file
+            DirectoryInfo dir  = new DirectoryInfo (@".\DOCS\Partner.csv");
+            if (!File.Exists(dir.FullName))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(dir.FullName));
+                File.Create(dir.FullName);
+            }
+        }
+
+
+
+
+    }
+}
