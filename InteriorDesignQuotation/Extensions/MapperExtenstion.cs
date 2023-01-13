a41a2196ab03038c39141aa349733a246c9cd75e
GENYA <t6589989@gmail.com>
2023-01-13 16:52:26 +0800

    feat: Add quotation save & load, add data model and mapper extension

---
+++ InteriorDesignQuotation/Extensions/MapperExtenstion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using InteriorDesignQuotation.Models;
+using InteriorDesignQuotation.Services.Interfaces;
+
+namespace InteriorDesignQuotation.Extensions;
+
+public static class MapperExtenstion
+{
+    public static ObservableCollection<WorkItemViewModel> ToWorkItemViewModel(this IList<WorkItemModel> quotation) =>
+        quotation.Select(x => new WorkItemViewModel
+        {
+            Category = x.Category,
+            Area = x.Area,
+            Name = x.Name,
+            Quantity = x.Quantity,
+            Unit = x.Unit,
+            UnitPrice = x.UnitPrice,
+            TotalPrice = x.TotalPrice,
+            Note = x.Note
+        }).ToObservableCollection();
+
+    public static IList<WorkItemModel> ToWorkItemModel(this ObservableCollection<WorkItemViewModel> workItemView) => workItemView.Select(x => new WorkItemModel
+    {
+        Category = x.Category,
+        Area = x.Area,
+        Name = x.Name,
+        Quantity = x.Quantity,
+        Unit = x.Unit,
+        UnitPrice = x.UnitPrice,
+        TotalPrice = x.TotalPrice,
+        Note = x.Note
+    }).ToList();
+}
